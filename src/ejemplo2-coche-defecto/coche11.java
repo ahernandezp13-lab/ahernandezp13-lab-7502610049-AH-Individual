f73bd086bac97e385aa4bfc9d76b8df618a71b68
ahernandezp13-lab <ahernandezp13@unicartagena.edu.co>
2026-09-18 21:04:12 -0500

    feat(ejemplo2): constructor por defecto

---
+++ src/ejemplo2-coche-defecto/coche11.java
@@ -0,0 +1,12 @@
+public class coche11 {
+    String marca;
+
+    public coche11() {
+        marca = "Desconocida";
+    }
+
+    public static void main(String[] args) {
+        coche c = new coche();
+        System.out.println("Marca: " + c.marca);
+    }
+}
