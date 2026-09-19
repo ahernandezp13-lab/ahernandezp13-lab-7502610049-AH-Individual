b5aa055fda1e2008f9ab1b9df5e055b6d7c2d30c
ahernandezp13-lab <ahernandezp13@unicartagena.edu.co>
2026-09-18 21:08:36 -0500

    feat(ejemplo3): constructor parametrizado

---
+++ src/ejemplo3-coche-parametrizado/coche2.java
@@ -0,0 +1,12 @@
+public class coche2 {
+    String marca;
+
+    public coche2(String marca) {
+        this.marca = marca;
+    }
+
+    public static void main(String[] args) {
+        coche c = new coche();
+        System.out.println("Marca: " + c.marca);
+    }
+}
